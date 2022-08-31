2264729b758de2e70f51d3366faaf0b23592f2c7
Anando Ghose <anandoghose@gmail.com>
2022-08-31 21:12:12 +0530

    landing

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,20 @@
+<script>
+  import logo from "../images/logo.svg";
+</script>
+
+<nav class="fixed top-0 left-0 right-0 lg:px-[75px]">
+  <div class="lg:pt-[100px] lg:flex lg:justify-between lg:items-start">
+
+    <p>
+      <img src={logo} alt="logo" class="h-[92px] w-[168px]" />
+    </p>
+    <div class="flex gap-x-5">
+    <div>
+      <button>menu</button>
+    </div>
+    <div>
+      <button>email maya</button>
+    </div>
+  </div>
+  </div>
+</nav>
