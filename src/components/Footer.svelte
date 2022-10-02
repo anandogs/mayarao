0c149e16b464848e61b282bff87561a7b5780a86
Anando Ghose <anandoghose@gmail.com>
2022-10-02 09:49:30 +0530

    footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,137 @@
+<script lang="ts">
+  import logo from "../images/logo.svg";
+</script>
+
+<footer>
+  <div class="logo">
+    <img src={logo} alt="logo" />
+  </div>
+  <div class="footer-wrapper-desktop">
+    <div class="footer-items">
+      <div>
+        <button>email maya</button>
+      </div>
+      <div>
+        <button>about maya</button>
+      </div>
+      <div class="footer-copyright">
+        <h4>copyright 2022 maya krishna rao/ vismayah productions</h4>
+        <div class="dot">
+          <h4><span>&#183;</span></h4>
+        </div>
+        <h4>all rights reserved.</h4>
+      </div>
+      <div>
+        <button>facebook</button>
+      </div>
+      <div>
+        <button>instagram</button>
+      </div>
+      <div class="website-by">
+        <h4>website by</h4>
+        <button>tide x tide</button>
+      </div>
+    </div>
+    <div class="website-by-desktop">
+        <h4>website by</h4>
+        <button>tide x tide</button>
+      </div>
+  </div>
+</footer>
+
+<style>
+  footer {
+    background-color: #020202;
+    color: white;
+    display: grid;
+
+    justify-content: center;
+    padding-bottom: 38px;
+  }
+  .logo {
+    filter: brightness(0) invert(1);
+    width: 81px;
+    margin: 38px auto;
+  }
+  .footer-items {
+    display: grid;
+    justify-content: center;
+    justify-items: center;
+    text-align: center;
+    row-gap: 25px;
+  }
+   h4 {
+    width: 208px;
+    font-weight: 469;
+    font-size: 10px;
+    line-height: 18px;
+    letter-spacing: 0.03em;
+  }
+  .footer-copyright {
+    display: grid;
+    row-gap: 20px;
+  }
+  .footer-copyright .dot {
+    display: none;
+  }
+  .website-by {
+    margin-top: 10px;
+    display: grid;
+    justify-items: center;
+    row-gap: 10px;
+  }
+  .website-by h4 {
+    line-height: 0px;
+  }
+  .website-by-desktop {
+    display: none;
+  }
+
+  @media (min-width: 1024px) {
+    footer {
+
+      justify-content: start;
+      justify-items: start;
+      padding: 0 75px 74px 75px;
+    }
+    .logo {
+      margin: 59px 0px 38px 0px;
+    }
+    .footer-wrapper-desktop {
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+    }
+    .footer-items {
+      display: flex;
+      column-gap: 30px;
+      align-items: flex-end;
+    }
+    .footer-items h4 {
+      width: fit-content;
+    }
+    .footer-copyright {
+      display: flex;
+      column-gap: 8px;
+      align-items: center;
+    }
+    .footer-copyright .dot {
+      display: flex;
+    }
+    .website-by {
+      display: none;
+    }
+    .website-by-desktop {
+    display: flex;
+    justify-content: flex-end;
+    align-items: flex-end;
+    column-gap: 10px;
+  }
+  .website-by-desktop h4 {
+    margin-left: 10vw;
+    width: fit-content;
+    
+  }
+  
+  }
+</style>
